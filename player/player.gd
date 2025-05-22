eb0c488bd5717af87ff9c25bfd4305a070bb7484
Gamedevenri <gamedevenri@gmail.com>
2025-05-22 23:27:35 +0300

    1.0.0.0

--- player/player.gd
+++ player/player.gd
@@ -4,8 +4,8 @@ extends CharacterBody3D
 var SPEED = 2.0
 const JUMP_VELOCITY = 4.5
 
-var animsvec:Vector2
-var currvec:Vector2
+var blendvector:Vector2
+var currentvector:Vector2
 
 
 @onready var anims = $AnimationTree
@@ -21,6 +21,9 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	# НЕ ЗАБУДЬТЕ ДОБАВИТЬ INPUT MAP В PROJECT->PROJECT SETTINGS->INPUT MAP
+	
 	var input_dir := Input.get_vector("d", "a", "s", "w")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -31,8 +34,8 @@ func _physics_process(delta: float) -> void:
 		velocity.z = lerp(velocity.z, 0.0, 0.05)
 
 
-	animsvec = Vector2(input_dir.x, input_dir.y).normalized()
-	currvec = currvec.move_toward(-animsvec, 4*delta)
-	anims.set("parameters/BlendSpace2D/blend_position",Vector2(currvec.x, -currvec.y)) 
+	blendvector = Vector2(input_dir.x, input_dir.y).normalized()
+	currentvector = currentvector.move_toward(-blendvector, 4*delta)
+	anims.set("parameters/BlendSpace2D/blend_position",Vector2(currentvector.x, -currentvector.y)) 
 
 	move_and_slide()
